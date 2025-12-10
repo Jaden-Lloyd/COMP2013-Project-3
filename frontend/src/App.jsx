aea6bf0e1b053e3d41054e7c7ecdfe9bc7445d14
Jaden-Lloyd <jaden_lloyd@outlook.com>
2025-12-10 13:18:05 -0500

    Set up basic layout of App.jsx and started NotAuthorized and PageNotFound Components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
 import "./App.css";
 import products from "./data/products";
 import GroceriesAppContainer from "./Components/GroceriesAppContainer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NotAuthorized from "./Components/NotAuthorized";
+import PageNotFound from "./Components/PageNotFound";
 
 function App() {
   return (
     <>
-      <GroceriesAppContainer products={products} />
+      <Router>
+        <Routes>
+          {/* <Route path="/" element={<LoginPage />} />
+          <Route path="/create-user" element={<RegisterPage />} /> */}
+          <Route path="/main" element={<GroceriesAppContainer />} />
+          {/* <Route path="/add-product" element={} />
+          <Route path="/edit-product" element={} /> */}
+          <Route path="/not-authorized" element={<NotAuthorized />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
     </>
   );
 }
